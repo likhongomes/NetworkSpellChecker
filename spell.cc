22f7474cd3d31561cd2db924b1ebcb3b999685a1
likhongomes <tug60499@temple.edu>
2019-04-04 23:38:07 -0400

    commented

--- spell.cc
+++ spell.cc
@@ -32,12 +32,6 @@ April 2nd, 2019
 
 using namespace std;
 
-struct sockaddr_in client;
-int clientLen = sizeof(client);
-int connectionPort;
-int connectionSocket, bytesReturned;
-char recvBuffer[BUF_LEN];
-//recvBuffer[0] = '\0';
 
 //Messages
 char* clientMessage = "Status Code: 200\nSuccessfully connected to Spell Server\n";
@@ -48,6 +42,12 @@ char* msgError = "I didn't get your message. ):\n";
 char* msgClose = "Goodbye!\n";
 
 //Global Variables
+struct sockaddr_in client;
+int clientLen = sizeof(client);
+int connectionPort;
+int connectionSocket, bytesReturned;
+char recvBuffer[BUF_LEN];
+
 vector<string> words;
 queue<int> sockets;
 queue<string> logQ;
@@ -68,19 +68,27 @@ void *writeLog(void *args);
 
 int main(int argc, char* argv[]){
 
+    /*
+    The main function does all the house keeping in this software. It looks for arguments passed in.
+    If the user did not pass in a port number or a dictionary as arguments, then the program chooses 
+    3207 as the default port number and chooses the dictionary in the OS as the default dictionary. 
+    Later, the main function opens a dictionary and puts it into a vector. Then it opens the port number
+    and created all of the necessary threads such as worker threads, log thread and connectionGetter thread.
+    */
+
     string dictionary = DEFAULT_DICTIONARY;
 
     //doing some house keeping over here, checking for port number and dictionary.
     if (argc == 1){
         cout << "No port number entered. Switching to default port 3207\n";
-        connectionPort = 3207;
+        connectionPort = 3207; // if no custom port number was entered, then by default 3207 is set as the port number.
     } else {
         connectionPort = atoi(argv[1]);
     }
     if (argc < 3) {
         cout << "No dictionary name entered, switching to DEFAULT_DICTIONARY\n";
     } else{
-        dictionary = argv[2];
+        dictionary = argv[2]; //if no dictionary is passed in as a referece, the dictionary in the OS is selected by default.
     }
 
     //Reading the dictionary and loading it into the queue.
@@ -128,7 +136,11 @@ int main(int argc, char* argv[]){
 
 
 
-
+/*
+This is the getConnection function, it runs on its own thread and keeps listening for incoming connection requests and
+assign a clientSocket port to it. The client sockets are then pushed into a sockets queue for workers to use. It keeps
+listening for new connenctions on a ontinious while loop
+*/
 void *getConnections(void *arg){
     
     while(true){
@@ -157,19 +169,25 @@ void *getConnections(void *arg){
     return NULL;
 }
 
-
+/*
+This is the worker thread that takes a clientSocket and process their requests. At first this function looks for a client socket 
+to connect to if the queue is empty, the client socket goes to sleep, otherwise it connects. It starts communication 
+and send and recieve data. It takes a word from client and looks up the word in the dictionary. If it is there
+the comparing function returns the word concatanated with OK or Misspelled. When it is done (the client presses ESC character)
+the client closes the connection and goes to accept another client socket. If the client queue is empty, it goes to sleep.
+*/
 void *worker(void *arg){
     int threadID = (long)arg;
     int clientSocket;
 
-    while(1){
+    while(1){ // this is the master loop
         if(sockets.empty()){
             pthread_mutex_lock(&m);
 
             pthread_cond_wait(&c, &m);
             pthread_mutex_unlock(&m);
         }        
-        while(!sockets.empty()){
+        while(!sockets.empty()){ // it looks for for open connection.
             
             pthread_mutex_lock(&m);
             clientSocket = sockets.front();
@@ -177,7 +195,7 @@ void *worker(void *arg){
             cout << "Thread " << threadID << " Servicing clientSocket " << clientSocket << endl; 
             pthread_mutex_unlock(&m);
 
-            while(true){
+            while(true){ //this is the main connection loop that keeps going until the client hits ESC.
                 send(clientSocket, msgPrompt, strlen(msgPrompt), 0);
                 //recv() will store the message from the user in the buffer, returning
                 //how many bytes we received.
@@ -194,12 +212,12 @@ void *worker(void *arg){
                     string comparator(recvBuffer);
                     string temp = "";
                     temp = compare(comparator, words);
-
+                    //Critical section starts
                     pthread_mutex_lock(&mLog);
                     logQ.push(temp);
                     pthread_cond_signal(&cLog);
                     pthread_mutex_unlock(&mLog);
-
+                    //End of critical section
                     char result[temp.length()];
                     strcpy(result,temp.c_str());
                     send(clientSocket, result, strlen(result), 0);
@@ -213,6 +231,10 @@ void *worker(void *arg){
 }
 
 
+/*
+This is the writeLog function that gets passed in to be executed in a thread. This function
+opens a file named log.txt and then read string from logQ and print it in the log.txt file.
+*/
 void *writeLog(void *args){
 
     ofstream file;
@@ -231,7 +253,12 @@ void *writeLog(void *args){
     
 }
 
-
+/*
+This function takes a string from the recvBuffer and take the vector reference of 
+the dictionary. At first the function strip off any extra character such as \n, \0 \t
+from the string and then str.compare() to compare the string with every single word in
+dictionary. Based on the result the dictionary retruns the string concatenated with OK or Misspelled.
+*/
 string compare(string str1, vector<string> words){
 
     string str = "";
